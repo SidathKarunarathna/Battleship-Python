e0973343b27ed91b5461870ea26352237f123fc0
SidathKarunarathna <Sidath21325@gmail.com>
2023-01-09 14:36:30 +0530

    Ships drawing added

---
+++ engine.py
@@ -0,0 +1,56 @@
+import random
+class Ship:
+    def __init__(self,size):
+        self.row = random.randrange(0,9)
+        self.col = random.randrange(0,9)
+        self.size=size
+        self.orientation = random.choice(["h","v"])
+        self.indexes = self.compute_indexes()
+
+    def compute_indexes(self):
+        start_index = self.row*10+self.col
+        if self.orientation == "h":
+            return[start_index + i for i in range(self.size)]
+        elif self.orientation== "v":
+            return[start_index + i*10 for i in range(self.size)]
+
+
+class Player:
+    def __init__(self):
+        self.ships =[]
+        self.search=["U" for i in range(100)]
+        self.place_ships(sizes=[5,4,3,3,2])
+        list_of_list=[ship.indexes for ship in self.ships]
+        self.indexes = [index for sublist in list_of_list for index in sublist]
+
+    def place_ships(self,sizes):
+        for size in sizes:
+            placed=False
+            while not placed:
+                ship = Ship(size)
+                possible =True
+                for i in ship.indexes:
+                    if i >=100:
+                        possible=False
+                        break
+                    new_row=i//10
+                    new_col=i%10
+                    if new_row!=ship.row and new_col!=ship.col:
+                        possible=False
+                        break
+
+                    for other_ship in self.ships:
+                        if i in other_ship.indexes:
+                            possible=False
+                            break
+                if possible:
+                    self.ships.append(ship)
+                    placed=True
+
+    def show_ships(self):
+        indexes=["-" if i not in self.indexes else "X" for i in range(100)]
+        for row in range(10):
+            print(" ".join(indexes[(row-1)*10:row*10]))
+
+p =Player()
+p.show_ships()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+from engine import Player
 pygame.init()
 pygame.display.set_caption("Battleship")
 
@@ -11,8 +11,10 @@ WIDTH =SQUARE_SIZE*10*2 + MARGIN_H
 HEIGHT = SQUARE_SIZE*10*2  + MARGIN_V
 SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
 
+INDENT =10
 GREY = (40,50,60)
 WHITE =(255,250,250)
+GREEN= (50,200,150)
 
 def draw_grid(left=0,top=0):
     for i in range(100):
@@ -21,8 +23,22 @@ def draw_grid(left=0,top=0):
         square= pygame.Rect(x,y,SQUARE_SIZE,SQUARE_SIZE)
         pygame.draw.rect(SCREEN,WHITE,square,width=3)
 
+def draw_ships(player,left=0,top=0):
+    for ship in player.ships:
+        x= left+ ship.col*SQUARE_SIZE +INDENT
+        y= top+ship.row*SQUARE_SIZE +INDENT
 
+        if ship.orientation =="h":
+            width=ship.size *SQUARE_SIZE -2*INDENT
+            height =SQUARE_SIZE -2*INDENT
+        else:
+            width=SQUARE_SIZE -2*INDENT
+            height=ship.size*SQUARE_SIZE -2*INDENT
+        rectangle = pygame.Rect(x,y,width,height)
+        pygame.draw.rect(SCREEN,GREEN,rectangle,border_radius=15)
 
+player1=Player()
+player2=Player()
 play=True
 pause=False
 while play:
@@ -37,10 +53,12 @@ while play:
 
     if not pause:
         SCREEN.fill(GREY)
-        draw_grid(left=20,top=20)
-        draw_grid(left=(WIDTH-MARGIN_H)//2+MARGIN_H-20,top = 20)
-        draw_grid(left=20,top=(HEIGHT-MARGIN_V)//2+MARGIN_V)
-        draw_grid(left=(WIDTH-MARGIN_H)//2+MARGIN_H-20,top=(HEIGHT-MARGIN_V)//2+MARGIN_V)
+        draw_grid(left=0,top=0)
+        draw_grid(left=(WIDTH-MARGIN_H)//2+MARGIN_H,top = 0)
+        draw_grid(left=0,top=(HEIGHT-MARGIN_V)//2+MARGIN_V)
+        draw_grid(left=(WIDTH-MARGIN_H)//2+MARGIN_H,top=(HEIGHT-MARGIN_V)//2+MARGIN_V)
+        draw_ships(player1,top=(HEIGHT-MARGIN_V)//2+MARGIN_V)
+        draw_ships(player2,left=(WIDTH-MARGIN_H)//2+MARGIN_H)
         pygame.display.flip()
 
 
